1533929848cac4eb03a06f5b610fb2e9a0641f46
Mohammad Turaab <45646831+mohammadturaab@users.noreply.github.com>
2022-04-16 12:50:25 -0500

    created models and superuser

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+GAME_TYPE = {
+    ('Basketball', 'Basketball'),
+    ('Football', 'Football'),
+    ('Baseball', 'Baseball'),
+    ('Volleyball', 'Volleyball'),
+    ('Soccer', 'Soccer'),
+}
+
+class Game(models.Model):
+    title = models.CharField(max_length=100)
+    game_type = models.CharField(max_length=50, choices=GAME_TYPE)
+    time = models.CharField(max_length=50)
+    date = models.CharField(max_length=50)
+    street = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zip = models.CharField(max_length=50)
+    img = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
+

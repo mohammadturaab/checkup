8e16a27d98450c625e56ba2c9de51c8fa684eb9f
Mohammad Turaab <45646831+mohammadturaab@users.noreply.github.com>
2022-04-16 15:01:41 -0500

    added game details path in urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('findgame/', views.FindGame.as_view(), name="findgame"),
-    path('creategame', views.CreateGame.as_view(), name="creategame"),
+    path('creategame/', views.CreateGame.as_view(), name="creategame"),
+    path('findgame/<int:pk>/', views.GameDetail.as_view(), name="gamedetails"),
 ]

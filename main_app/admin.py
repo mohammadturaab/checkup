7f6de50b82fcd219d976202109d5c0bf147cb52f
Mohammad Turaab <45646831+mohammadturaab@users.noreply.github.com>
2022-04-19 18:11:18 -0500

    added group to admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Game
+from .models import Game, Group
 
 # Register your models here.
 
-admin.site.register
+admin.site.register(Game)
+admin.site.register(Group)

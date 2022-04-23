5c8800c294bccb417b69791c979e73dc129065e7
Mohammad Turaab <45646831+mohammadturaab@users.noreply.github.com>
2022-04-23 15:12:22 -0500

    updated url path

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('/', views.Home.as_view(), name="home"),
+    path('', views.Home.as_view(), name="home"),
     path('findgame/', views.FindGame.as_view(), name="findgame"),
     path('creategame/', views.CreateGame.as_view(), name="creategame"),
     path('findgame/<int:pk>/', views.GameDetail.as_view(), name="gamedetail"),

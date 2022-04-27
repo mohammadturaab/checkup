16a08e9241afa281675e46e2ac595767cff636bf
Mohammad Turaab <45646831+mohammadturaab@users.noreply.github.com>
2022-04-27 17:08:04 -0500

    imported django

--- checkup/settings.py
+++ checkup/settings.py
@@ -133,5 +133,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/findgame/'
 LOGOUT_REDIRECT_URL = '/'
 
-# import django_heroku
-# django_heroku.settings(locals())
+import django_heroku
+django_heroku.settings(locals())
